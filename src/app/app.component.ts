5ef82f3d32c2162654d5ed0d065498e328f5b561
lkre <lukasz.res@gmail.com>
2021-06-25 11:24:41 +0200

    zad 3 dyrektywa *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isMyComponentVisible: boolean = true;
 
+  toggleComponent(): void{
+    this.isMyComponentVisible = !this.isMyComponentVisible;
+  }
 }
