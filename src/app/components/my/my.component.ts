d1a019411fddd28bda7a201257860e7316170619
lkre <lukasz.res@gmail.com>
2021-06-25 11:20:23 +0200

    zad 2 nowy komponent

---
+++ src/app/components/my/my.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-component',
+  templateUrl: './my.component.html',
+  styleUrls: ['./my.component.css']
+})
+export class MyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
