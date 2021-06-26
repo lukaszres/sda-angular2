8af8d76227a2391fe96252012eb0a5db7e04b90a
lkre <lukasz.res@gmail.com>
2021-06-26 07:05:50 +0200

    zad 21 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,53 @@ import {MyDirective} from "./directives/my.directive";
 import {ModuloPipe} from "./pipes/modulo.pipe";
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
+import { LoginComponent } from './components/login/login.component';
+import { NewsComponent } from './components/news/news.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const appRotes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {page: 'Home page'}
+  },
+  {
+    path: 'news/:newsId',
+    component: NewsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComponent,
     MyDirective,
-    ModuloPipe
+    ModuloPipe,
+    HomeComponent,
+    LoginComponent,
+    NewsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRotes)
   ],
   providers: [],
   bootstrap: [AppComponent]
